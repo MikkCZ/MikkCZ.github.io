f76526dba570a22fb8d0274035361b4b32e363d2
Michal Stanke <michal@stanke.cz>
2016-12-28 14:33:46 +0100

    Rewrite l10n-switch.js to OOP, fix #4

--- js/l10n-switch.js
+++ js/l10n-switch.js
@@ -1,55 +1,73 @@
-var l10nswitch = function() {
-    var addLangSwitcher = function(langsDiv, lang, l10nID) {
-        var link = document.createElement('a');
-        link.href = '#'+lang;
-        link.setAttribute('data-l10n-id', l10nID);
-        var flag = document.createElement('img');
-        flag.src = 'images/'+lang+'.jpg';
-        link.appendChild(flag);
-        langsDiv.appendChild(link);
-    }
+if (typeof l10nswitch === "undefined") {
+    var l10nswitch = {};
+}
 
-    var header = document.body.getElementsByTagName('header')[0];
+l10nswitch.L10Nswitch = function() {
+    this._langs = this._addLangs();
+    this._setLang(window.location.hash.split('#')[1] || navigator.languages[0]);
+}
+
+l10nswitch.L10Nswitch.prototype._addLangs = function() {
     var langsDiv = document.createElement('div');
     langsDiv.id = 'langs';
-    addLangSwitcher(langsDiv, 'en', 'english');
-    addLangSwitcher(langsDiv, 'cs', 'czech');
+    this._addLang(langsDiv, 'en', 'english');
+    this._addLang(langsDiv, 'cs', 'czech');
+
+    var header = document.body.getElementsByTagName('header')[0];
     header.insertBefore(langsDiv, header.firstChild);
+
     var langs = langsDiv.children;
+    this._addLangClickListener(langs);
+    return langs;
+}
 
-    var setLang = function(lang) {
-        if (!lang) {
-            document.l10n.requestLanguages();
-        } else {
-            document.l10n.requestLanguages([lang]);
-        }
-        for (var i = 0; i < langs.length; i++) {
-            var a = langs[i];
-            while (a.nodeName.toLowerCase() !== 'a') {
-                a = a.parentNode;
-            }
-            if (a.href.split('#')[1] == lang) {
-                a.style.display = 'none';
-            } else {
-                a.style.display = 'inline';
-            }
-        }
-    };
+l10nswitch.L10Nswitch.prototype._addLang = function(langsDiv, lang, l10nID) {
+    var flag = document.createElement('img');
+    flag.src = 'images/'+lang+'.jpg';
 
-    var langClickListener = function(e) {    
-        var a = e.target;
+    var link = document.createElement('a')
+    link.href = '#'+lang;
+    link.setAttribute('data-l10n-id', l10nID);
+    link.appendChild(flag);
+
+    langsDiv.appendChild(link);
+}
+
+l10nswitch.L10Nswitch.prototype._addLangClickListener = function(langs) {
+    var thisLangClickListener = this._langClickListener.bind(this);
+    for (var i = 0; i < langs.length; i++) {
+        langs[i].addEventListener('click', thisLangClickListener);
+    }
+}
+
+l10nswitch.L10Nswitch.prototype._langClickListener = function(e) {
+    var a = e.target;
+    while (a.nodeName.toLowerCase() !== 'a') {
+        a = a.parentNode;
+    }
+    this._setLang(a.href.split('#')[1]);
+}
+
+l10nswitch.L10Nswitch.prototype._setLang = function(lang) {
+    if (!lang) {
+        document.l10n.requestLanguages();
+    } else {
+        document.l10n.requestLanguages([lang]);
+    }
+    for (var i = 0; i < this._langs.length; i++) {
+        var a = this._langs[i];
         while (a.nodeName.toLowerCase() !== 'a') {
             a = a.parentNode;
         }
-        setLang(a.href.split('#')[1]);
-    };
-
-    for (var i = 0; i < langs.length; i++) {
-        langs[i].addEventListener('click', langClickListener);
+        if (a.href.split('#')[1] == lang) {
+            a.style.display = 'none';
+        } else {
+            a.style.display = 'inline';
+        }
     }
-
-    setLang(window.location.hash.split('#')[1] || navigator.languages[0]);
 }
 
-window.addEventListener('DOMContentLoaded', l10nswitch);
+window.addEventListener('DOMContentLoaded', function() {
+    new l10nswitch.L10Nswitch();
+});
 
